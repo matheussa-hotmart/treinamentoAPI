5a2373910d4d85585a4b7f4fe64ee80450e76169
matheussa-hotmart <matheus-sa@hotmart.com>
2021-10-26 15:53:02 -0300

    fix FuncionarioController

--- src/main/java/br/com/hotmart/apiteste/controller/FuncionarioController.java
+++ src/main/java/br/com/hotmart/apiteste/controller/FuncionarioController.java
@@ -2,7 +2,6 @@ package br.com.hotmart.apiteste.controller;
 
 import br.com.hotmart.apiteste.dto.FuncionarioDTO;
 import br.com.hotmart.apiteste.form.FuncionarioForm;
-import br.com.hotmart.apiteste.model.Funcionario;
 import br.com.hotmart.apiteste.repository.EnderecoRepository;
 import br.com.hotmart.apiteste.repository.FuncionarioRepository;
 import br.com.hotmart.apiteste.service.FuncionarioService;
@@ -32,23 +31,23 @@ public class FuncionarioController {
     }
 
     @GetMapping("/{id}")
-    public FuncionarioDTO getOneFuncionario(@PathVariable("id") Long id){
-        return new FuncionarioDTO(funcionarioService.getOneFuncionario(id));
+    public ResponseEntity<FuncionarioDTO> getOneFuncionario(@PathVariable("id") Long id){
+        return ResponseEntity.ok(new FuncionarioDTO(funcionarioService.getOneFuncionario(id)));
     }
 
     @PostMapping
-    public FuncionarioDTO createFuncionario(@RequestBody @Valid FuncionarioForm form){
-        return new FuncionarioDTO(funcionarioService.createFuncionario(form));
+    public ResponseEntity<FuncionarioDTO> createFuncionario(@RequestBody @Valid FuncionarioForm form){
+        return ResponseEntity.ok(new FuncionarioDTO(funcionarioService.createFuncionario(form)));
     }
 
     @PutMapping("/{id}")
-    public  FuncionarioDTO updateFuncionario(@PathVariable("id") Long id, @RequestBody @Valid FuncionarioForm form){
-        return new FuncionarioDTO(funcionarioService.updateFuncionario(id, form));
+    public  ResponseEntity<FuncionarioDTO> updateFuncionario(@PathVariable("id") Long id, @RequestBody @Valid FuncionarioForm form){
+        return ResponseEntity.ok(new FuncionarioDTO(funcionarioService.updateFuncionario(id, form)));
     }
 
     @DeleteMapping("/{id}")
-    public FuncionarioDTO safeDelete(@PathVariable("id") Long id){
-        return new FuncionarioDTO(funcionarioService.safeDeleteFuncionario(id));
+    public ResponseEntity<FuncionarioDTO> safeDelete(@PathVariable("id") Long id){
+        return ResponseEntity.ok(new FuncionarioDTO(funcionarioService.safeDeleteFuncionario(id)));
     }
 
 }
